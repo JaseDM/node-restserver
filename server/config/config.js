29a92ffc1463b846ec3a29aa9cab8a3c8449acc2
JaseDm <josemi.fotografo@gmail.com>
2020-11-29 16:43:23 +0100

    Cambios archivo de configuración

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-//if( process.env.NODE_ENV === 'dev' ){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
+if( process.env.NODE_ENV === 'dev' ){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = 'mongodb+srv://JaseDm:xsDDbvKBMv6wbfWk@cluster0.dbhv4.mongodb.net/cafe';
-//}
+}
 
 process.env.URLDB = urlDB;
 
