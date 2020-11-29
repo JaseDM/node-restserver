081a71659334dab658db92ac2eafe6b2edd331b9
JaseDm <josemi.fotografo@gmail.com>
2020-11-30 00:13:47 +0100

    Archivo de configuración de rutas globales y variable de entorno con el seed de desarrollo

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use( require('./routes/usuario') );
+
 
  
 
- 
+app.use( require('./routes/index') );
 
 
 
